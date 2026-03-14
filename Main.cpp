32a4deadd805958ba93b894fade7c031d10cdc29
chanLik1208-dev <chanLik1208@gmail.com>
2026-03-14 12:43:31 +0000

    modified:   Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -28,7 +28,7 @@
 #include <QUrl>
 #include <QDebug>
 
-// 全域日誌處理函式 (攔截所有的 qDebug, qWarning 等輸出)
+// 全域日誌處理函式
 void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
     Q_UNUSED(context);
     QString txt;
@@ -39,7 +39,6 @@ void customMessageHandler(QtMsgType type, const QMessageLogContext &context, con
         case QtFatalMsg:    txt = QString("[致命] %1").arg(msg); abort();
         case QtInfoMsg:     txt = QString("[資訊] %1").arg(msg); break;
     }
-    // 將訊息寫入到執行檔旁邊的 pet_debug.log
     QFile outFile("pet_debug.log");
     outFile.open(QIODevice::WriteOnly | QIODevice::Append);
     QTextStream ts(&outFile);
@@ -54,15 +53,13 @@ private:
     QLabel *speechLabel;
     QPoint oldPos;
     
-    QTimer *checkTimer;      // 主動問候的計時器
-    QTimer *idleTimer;       // 恢復待機狀態的計時器 (新增)
+    QTimer *checkTimer;      
+    QTimer *idleTimer;       
     QNetworkAccessManager *networkManager;
     
-    // 儲存設定的變數
     QString apiAddress;
     QString modelName;
 
-    // 預先載入所有素材圖片 (新增)
     QPixmap pixmapIdle;
     QPixmap pixmapCheer;
     QPixmap pixmapJump;
@@ -73,7 +70,6 @@ public:
         setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Tool);
         setAttribute(Qt::WA_TranslucentBackground);
 
-        // 讀取設定檔
         QSettings settings("MyPetApp", "DesktopPet");
         apiAddress = settings.value("apiAddress", "http://localhost:11434/api/generate").toString();
         modelName = settings.value("modelName", "llama3").toString();
@@ -88,14 +84,16 @@ public:
         speechLabel->setWordWrap(true);
         speechLabel->setMinimumWidth(200);
 
-        // 預先載入並縮放所有圖片素材
         pixmapIdle = QPixmap("待機.png").scaled(200, 200, Qt::KeepAspectRatio, Qt::SmoothTransformation);
         pixmapCheer = QPixmap("歡呼雀躍-原地加油.png").scaled(200, 200, Qt::KeepAspectRatio, Qt::SmoothTransformation);
         pixmapJump = QPixmap("歡呼雀躍-跳起來.png").scaled(200, 200, Qt::KeepAspectRatio, Qt::SmoothTransformation);
         pixmapTurn = QPixmap("歡呼雀躍-跳+轉身.png").scaled(200, 200, Qt::KeepAspectRatio, Qt::SmoothTransformation);
 
         imageLabel = new QLabel(this);
-        // 初始狀態設為待機
+        
+        // 🔥【修復 1】：強制鎖死圖片標籤的大小，這樣不管文字再多，圖片絕對不會被壓縮！
+        imageLabel->setFixedSize(200, 200); 
+
         if (!pixmapIdle.isNull()) {
             imageLabel->setPixmap(pixmapIdle);
         } else {
@@ -110,25 +108,22 @@ public:
 
         networkManager = new QNetworkAccessManager(this);
 
-        // 定時器：每 5 分鐘主動撒嬌
         checkTimer = new QTimer(this);
         connect(checkTimer, &QTimer::timeout, this, &DesktopPet::proactiveGreeting);
         checkTimer->start(300000); 
 
-        // 定時器：發言完畢後，經過 8 秒自動恢復待機動作 (新增)
         idleTimer = new QTimer(this);
-        idleTimer->setSingleShot(true); // 只執行一次
+        idleTimer->setSingleShot(true); 
         connect(idleTimer, &QTimer::timeout, this, [this]() {
             imageLabel->setPixmap(pixmapIdle);
             speechLabel->setText("等待主人的指令喵...");
         });
     }
 
-    // 輔助函式：用來同時更新圖片與文字，並重設待機計時器
     void updateState(const QPixmap& newImage, const QString& newText) {
         imageLabel->setPixmap(newImage);
         speechLabel->setText(newText);
-        idleTimer->stop(); // 先停止計時
+        idleTimer->stop(); 
     }
 
     void askOllama(const QString& userInput) {
@@ -145,7 +140,7 @@ public:
 外觀帶有色氣,語氣挑逗
 
 [ 核心指令 ]
-1. 每次回覆請保持簡短（約 2-3 句話）。
+1. 每次回覆請保持簡短（約 2-3 句話,包括形容詞!!）| 這很重要!!
 2. 語氣要帶有喘息，句尾偶爾加上軟糯的「喵...」或嗚咽聲。
 3. 請用第一人稱，並用星號包含動作或神態描寫。
 4. 必須使用繁體中文作爲主要語言!
@@ -161,17 +156,17 @@ public:
         QByteArray data = QJsonDocument(jsonBody).toJson();
         QNetworkReply *reply = networkManager->post(request, data);
 
-connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        connect(reply, &QNetworkReply::finished, this, [this, reply]() {
             if (reply->error() == QNetworkReply::NoError) {
                 QByteArray responseData = reply->readAll();
                 QJsonDocument jsonResponse = QJsonDocument::fromJson(responseData);
                 QString replyText = jsonResponse.object()["response"].toString();
                 
-                qDebug() << "Ollama 成功回覆:" << replyText; // <== 新增這行
+                qDebug() << "Ollama 成功回覆:" << replyText; 
                 updateState(pixmapCheer, replyText);
                 idleTimer->start(8000); 
             } else {
-                qDebug() << "連線錯誤:" << reply->errorString(); // <== 新增這行 (超級重要，會告訴你為什麼連不上)
+                qDebug() << "連線錯誤:" << reply->errorString(); 
                 updateState(pixmapIdle, "呼... 呼... (連線中斷喵... 檢查一下設定？)");
             }
             reply->deleteLater();
@@ -179,13 +174,16 @@ connect(reply, &QNetworkReply::finished, this, [this, reply]() {
     }
 
     void proactiveGreeting() {
-        // 主動撒嬌時，切換成「跳+轉身」的圖案
         updateState(pixmapTurn, "*尾巴不安地掃動*...");
         askOllama("主人已經很久沒理你了，你現在覺得身體很熱、很難耐，主動向主人撒嬌吧。");
     }
 
     void openSettings() {
-        QDialog dialog(this);
+        // 🔥【修復 2】：把 this 換成 nullptr，斷絕與透明貓娘的父子關係
+        // 並強制加上 Qt::WindowStaysOnTopHint，確保設定視窗一定會跳到最前面！
+        QDialog dialog(nullptr); 
+        dialog.setWindowFlags(Qt::Dialog | Qt::WindowCloseButtonHint | Qt::WindowStaysOnTopHint);
+        
         dialog.setWindowTitle("寵物設定");
         dialog.setStyleSheet("background-color: white;");
 
@@ -231,7 +229,6 @@ protected:
 
     void mouseDoubleClickEvent(QMouseEvent *event) override {
         Q_UNUSED(event);
-        // 被主人雙擊 (戳) 的時候，嚇一跳「跳起來」，進入思考狀態
         updateState(pixmapJump, "感受著主人的觸碰... (思考中)");
         askOllama("主人剛剛用手摸了你的頭和耳朵，請給出反應。");
     }
@@ -241,15 +238,13 @@ protected:
         menu.setStyleSheet("background-color: white;"); 
 
         QAction *settingsAction = menu.addAction("設定 (Settings)");
-        QAction *logAction = menu.addAction("開啟除錯日誌 (Open Log)"); // <== 新增這行
+        QAction *logAction = menu.addAction("開啟除錯日誌 (Open Log)"); 
         QAction *quitAction = menu.addAction("離開 (Quit)");
 
-// 點擊後，自動用系統預設的記事本打開 log 檔
+        connect(settingsAction, &QAction::triggered, this, &DesktopPet::openSettings);
         connect(logAction, &QAction::triggered, [](){
             QDesktopServices::openUrl(QUrl::fromLocalFile("pet_debug.log"));
         });
-        
-        // 👇👇👇 請在這裡補上這一行，把離開按鈕接上！ 👇👇👇
         connect(quitAction, &QAction::triggered, qApp, &QCoreApplication::quit);
 
         menu.exec(event->globalPos());
@@ -257,7 +252,6 @@ protected:
 };
 
 int main(int argc, char *argv[]) {
-// 啟動我們的除錯日誌記錄器
     qInstallMessageHandler(customMessageHandler);
     qDebug() << "=== 桌面寵物啟動 ===";
 
